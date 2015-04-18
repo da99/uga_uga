5efcb3865edbae7e5749cc75135c1cb5f1651b2e
da99 <i-hate-spam-1234567@mailinator.com>
2015-04-17 20:42:59 -0500

    Dev. checkpoint: Touchup.

--- README.md
+++ README.md
@@ -10,4 +10,42 @@ Use [Treetop](https://github.com/nathansobo/treetop) or [OMeta](https://github.c
 
 ## Usage
 
-Not coming any time soon.
+```ruby
+  require "uga_uga"
+
+  code = <<-EOF
+    bobby {
+      howie { :funny }
+      mandel { "comedian" }
+    }
+  EOF
+
+  results = []
+
+  Uga_Uga.uga code do |name, blok|
+
+    case name
+
+    when "bobby"
+      results << "bobby was called"
+      blok
+
+    when "howie"
+      results << "howie was called"
+      blok
+
+    when "mandel"
+      results << "mandel was called"
+      blok
+
+    when ":funny", '"comedian"'
+
+    else
+      fail ArgumentError, "Command not found: #{name.inspect}"
+    end
+
+  end
+
+  puts results.inspect
+
+```
--- specs/0000-it-runs.rb
+++ specs/0000-it-runs.rb
@@ -13,6 +13,15 @@ describe "Uga_Uga" do
     ]
   end
 
+  it "runs code from README.md" do
+    file     = File.expand_path(File.dirname(__FILE__) + '/../README.md')
+    contents = File.read( file )
+    code     = (contents[/```ruby([^`]+)```/] && $1).gsub('puts ','')
+
+    result = eval(code, nil, file, contents.split("\n").index('```ruby') + 1)
+    result.should.match /bobby/
+  end # === it
+
   it "accepts commands" do
     result = []
     Uga_Uga.uga "p { }" do |cmd, block|
@@ -46,8 +55,8 @@ describe "Uga_Uga" do
     code = <<-EOF
       p {
         span {
-          1 { 
-            2 { 
+          1 {
+            2 {
               3 { "my text" }
             }
           }
@@ -65,6 +74,21 @@ describe "Uga_Uga" do
     end
 
     result.should == [:p, :span, :'1', :'2', :'3', '"my text"']
+  end # === it
+
+  it "passes all text before a block start: cmd, cmd {" do
+    result = []
+    code = %^
+      cmd, cmd {
+        inner, inner {
+        }
+      }
+    ^
+    Uga_Uga.uga(code) do |cmd, code|
+      result << cmd
+      code
+    end
+    result.should == ["cmd, cmd", "inner, inner"]
   end # === it
 
 end # === describe "Uga_Uga"
