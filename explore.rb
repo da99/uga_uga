993335fca904ef303c9b01ecf74d3f7fa545a44b
da99 <i-hate-spam-1234567@mailinator.com>
2015-04-19 12:45:21 -0500

    Started: new design: customizable end tags/blocks

--- explore.rb
+++ explore.rb
@@ -12,84 +12,89 @@ class Uga_Uga
     end
   end
 
-  def is_command? l
-    case
+  class << self
 
-    when l == ""
-      :ignore
+    def is_command? l, *args
+      case
 
-    when l[/\A\ *Text\ *([^\ ]+)\ *\Z/]
-      eos = /\A\ *#{Regexp.escape $1}\ *\Z/
+      when l == ""
+        :ignore
 
-    when l[/\A(.+?)\ (?<!\{)\{(?!\{)(.+)(?<!\})\}(?!\})\ *\Z/] # eg: my command {{var}} { osmosdmf df}
-      {
-        :command => $1,
-        :block   => $2
-      }
+      when l[/\A\ *Text\ *([^\ ]+)\ *\Z/]
+        /\A\ *#{Regexp.escape $1}\ *\Z/
 
-    when l[/[^\{]\{\ *\Z/] # ... {
-      data << (blok = {:command=> l.sub(/\{\ *\Z/, ''), :block=>[]})
-      index = l.index(/[^\ ]/)
-      (' ' * index) + '}'
+      when l[/\A(.+?)\ (?<!\{)\{(?!\{)(.+)(?<!\})\}(?!\})\ *\Z/] # eg: my command {{var}} { osmosdmf df}
+        {
+          :command => $1,
+          :block   => $2
+        }
 
-    else
-      :insert
-
-    end # === case
-  end # === def is_command?
-
-  def uga str, is_command = :is_command?
-
-    stack = []
-    data  = {}
-    lines = str.split(NEW_LINE_REGEXP)
-
-    while l = lines.shift
-      action = case is_command
-               when Symbol
-                 send(is_command, l, data)
-               when Proc
-                 is_command.call l, stack, data
-               else
-                 fail ArgumentError, "Unknown class: #{is_command.inspect}"
-               end
-
-      case action
-
-      when :ignore
-
-      when :insert
-        stack << l
-
-      when Hash
-        stack << action
-
-      when String, Regexp
-        blok = []
-        found = false
-
-        cmd = l
-        while !found && ( l = lines.shift )
-          found = action.is_a?(String) ?
-            (l == action) :
-            (l =~ Regexp)
-          (blok << l) unless found
-        end
-
-        stack.<<(
-          {
-            :command => cmd,
-            :block   => blok
-          }
-        )
+      when l[/[^\{]\{\ *\Z/] # ... {
+        index = l.index(/[^\ ]/)
+        (' ' * index) + '}'
 
       else
-        fail ArgumentError, "Unknown action from #{is_command}: #{action.inspect}"
-      end
+        :insert
 
-    end # === while l
+      end # === case
+    end # === def is_command?
 
-  end # === def uga
+    def uga str, is_command = :is_command?
+
+      stack = []
+      data  = {}
+      lines = str.split(NEW_LINE_REGEXP)
+
+      while l = lines.shift
+        action = case is_command
+                 when Symbol
+                   send(is_command, l, data)
+                 when Proc
+                   is_command.call l, stack, data
+                 else
+                   fail ArgumentError, "Unknown class: #{is_command.inspect}"
+                 end
+
+        case action
+
+        when :ignore
+
+        when :insert
+          stack << l
+
+        when Hash
+          stack << action
+
+        when String, Regexp
+          blok = []
+          found = false
+
+          cmd = l
+
+          while !found && ( l = lines.shift )
+            found = action.is_a?(String) ?
+              (l == action) :
+              (l =~ action)
+            (blok << l) unless found
+          end
+
+          stack.<<(
+            {
+              :command => cmd,
+              :block   => blok
+            }
+          )
+
+        else
+          fail ArgumentError, "Unknown action from #{is_command}: #{action.inspect}"
+        end
+
+      end # === while l
+
+      stack
+    end # === def uga
+
+  end # === class << self
 
 end # === class Uga_Uga
 
@@ -136,46 +141,14 @@ puts "================================"
 puts str
 puts "================================"
 
-while l = lines.shift
-  case
 
-  when l == ""
-    # do nothing
+is_command = lambda { |l, *args|
+  Uga_Uga.is_command? l, *args
+}
+
+results = Uga_Uga.uga str, is_command
+ap(results)
 
 
-  when l[/\A\ *Text\ *([^\ ]+)\ *\Z/]
-    eos = /\A\ *#{Regexp.escape $1}\ *\Z/
-    text = []
-    while lines.first && lines.first !~ eos
-      text << lines.shift
-    end
-    lines.shift
-    data << {:command => :string, :block => text.join("\n")}
 
-  when l[/\A(.+?)\ (?<!\{)\{(?!\{)(.+)(?<!\})\}(?!\})\ *\Z/] # eg: my command {{var}} { osmosdmf df}
-    data << {
-      :command => $1,
-      :block   => $2
-    }
 
-  when l[/[^\{]\{\ *\Z/] # ... {
-
-    data << (blok = {:command=> l.sub(/\{\ *\Z/, ''), :block=>[]})
-    index = l.index(/[^\ ]/)
-    found = false
-    while !found && ( blok_l = lines.shift )
-      brace = blok_l.index '}'
-      if brace && brace == (index)
-        (found = true)
-      else
-        blok[:block] << blok_l
-      end
-    end
-
-  else
-    data << l
-
-  end # === case
-end
-
-ap data
